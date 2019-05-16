0416a412dd804433c11824c1d85e16e1f9369711
mingzi12 <690632338@qq.com>
2019-05-16 10:27:20 +0800

    使用mybatis的when标签和otherwise标签实现动态拼接sql查询。

--- src/test/java/com/hmx/mybatis/UserDaoImplTest.java
+++ src/test/java/com/hmx/mybatis/UserDaoImplTest.java
@@ -47,6 +47,30 @@ public class UserDaoImplTest {
         }
     }
 
+
+    @Test
+    public void getUsersTest(){
+        /**
+         * 映射sql的标识字符串，
+         * com.hmx.mybatis.mapping.UserMapping是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getAllUsers是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "com.hmx.mybatis.mapping.UserMapping.getUsers";//映射sql的标识字符串
+        //执行查询返回一个唯一user对象的sql
+
+        SqlSession session = SqlSessionUtil.getSqlSession();
+        Map<String, Object> param = new HashMap<>(10);
+        //如果有多个条件参数，则xml文件中第一个条件才会拼接
+        //param.put("name", "user");
+        param.put("age", 30);
+        List<User> userList = session.selectList(statement, param);
+        SqlSessionUtil.close(session);
+        for (User user : userList) {
+
+            System.out.println(user);
+
+        }
+    }
     @Test
     public void insertUserTest(){
         /**
